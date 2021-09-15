df11f3bf98e1ca7a4abcfae531a11736734f7d6c
Imran Hossain <faraazhossainimran@gmail.com>
2021-09-16 04:13:46 +0600

    Material Ui  ThemeProvider added

--- components/Layout.js
+++ components/Layout.js
@@ -1,8 +1,8 @@
 import {
   AppBar,
-  Container,
-  Link,
-  Toolbar,
+  Container, createMuiTheme,
+  CssBaseline,
+  Link, ThemeProvider, Toolbar,
   Typography
 } from '@material-ui/core';
 import Head from 'next/head';
@@ -11,6 +11,29 @@ import React from 'react';
 import useStyles from '../utils/styles';
 
 export default function Layout({ title, description, children }) {
+  const theme = createMuiTheme({
+    typography: {
+      h1: {
+        fontSize: '1.6rem',
+        fontWeight: 400,
+        margin: '1rem 0',
+      },
+      h2: {
+        fontSize: '1.4rem',
+        fontWeight: 400,
+        margin: '1rem 0',
+      },
+    },
+    palette: {
+      type: 'light',
+      primary: {
+        main: '#f0c000',
+      },
+      secondary: {
+        main: '#808080',
+      },
+    },
+  });
   const classes = useStyles();
   return (
     <div>
@@ -18,6 +41,8 @@ export default function Layout({ title, description, children }) {
         <title>{title ? `${title} -Ezintle.com` : 'Ezintle.com'} </title>
         {description && <meta name="description" content={description}></meta>}
       </Head>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
       <AppBar position="static" className={classes.navbar}>
         <Toolbar>
           <NextLink href="/" passHref>
@@ -38,6 +63,7 @@ export default function Layout({ title, description, children }) {
       <footer className={classes.footer}>
         <Typography>All Right Reserved. @Ecommerce</Typography>
       </footer>
+    </ThemeProvider>
     </div>
   );
 }
